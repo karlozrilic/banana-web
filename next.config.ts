58bda80cf06ac0048276f9d85973bcf3659fe935
karlozrilic <karlozrilic@gmail.com>
2026-08-21 21:07:53 +0200

    update

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+	output: 'export',
+	images: {
+		unoptimized: true,
+	},
+	basePath: '',
+	assetPrefix: '',
+	reactStrictMode: true,
+	experimental: {
+		optimizeCss: false,
+	}
 };
 
 export default nextConfig;
